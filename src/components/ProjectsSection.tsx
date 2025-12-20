85a74c06ccbf27c1fb00164ef2e4e10733b82a05
AJEET PRAKASH PANDEY <ajeetprpandey2701@gmail.com>
2025-12-20 19:03:43 +0530

    Update ProjectsSection.tsx

--- src/components/ProjectsSection.tsx
+++ src/components/ProjectsSection.tsx
@@ -21,7 +21,7 @@ const projects = [
     description: 'An interactive chess game where two AI agents powered by OpenAI GPT-4o-mini play against each other using the AutoGen framework.',
     image: projectAichess,
     technologies: ['Python'],
-    category: 'Full-Stack', 'AI/ML',
+    category: ['Full-Stack', 'AI/ML'],
     github: 'https://github.com/ajeetprakash27/Chess-Game',
   },
   {
